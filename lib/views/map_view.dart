0b6d1ac93975a6641748ab43eb436242ae1837eb
IvanParada <ivan.parada2001@alumnos.ubiobio.cl>
2023-06-03 23:49:17 -0400

    restriccionMapa-ZoomIn&ZoomOut

--- lib/views/map_view.dart
+++ lib/views/map_view.dart
@@ -21,6 +21,15 @@ class MapView extends StatelessWidget {
     const CameraPosition initialCameraPosition = CameraPosition(
         target: LatLng(-36.8220178016745, -73.0129262889358), zoom: 18);
 
+    CameraTargetBounds cameraTargetBounds = CameraTargetBounds(
+      LatLngBounds(
+        southwest: const LatLng(-36.825952992476,
+            -73.01679473201108), // Punto suroeste del área visible
+        northeast: const LatLng(-36.82006998398947,
+            -73.00732116939754), // Punto noreste del área visible
+      ),
+    );
+
     final size = MediaQuery.of(context).size;
 
     return SizedBox(
@@ -30,12 +39,16 @@ class MapView extends StatelessWidget {
         onPointerMove: (_) => mapBloc.add(OnStopFollowingUserEvent()),
         child: GoogleMap(
           initialCameraPosition: initialCameraPosition,
+          cameraTargetBounds: cameraTargetBounds,
           compassEnabled: false,
           myLocationEnabled: true,
           myLocationButtonEnabled: false,
           zoomControlsEnabled: false,
           polylines: polylines,
+          zoomGesturesEnabled: true,
+          minMaxZoomPreference: const MinMaxZoomPreference(16.0, 19.0),
           markers: markers,
+          rotateGesturesEnabled: true,
           onMapCreated: (controller) =>
               mapBloc.add(OnMapInitializedEvent(controller)),
           onCameraMove: (position) => mapBloc.mapCenter = position.target,
