adfdf22933605ab9d018e5a73a862acc0bc1e2b3
Ivan Parada <inzkars21@gmail.com>
2024-03-25 17:56:19 -0300

    curvedNavBar

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,6 +1,5 @@
-import 'package:animate_do/animate_do.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:ubb/screens/screens.dart';
 
 class MainScreen extends StatefulWidget {
@@ -13,58 +12,46 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   int selectedIndex = 0;
 
+  final screens = [
+    const HomeScreen(),
+    const MapsOptionsScreen(),
+    const SettingsScreen(),
+  ];
   @override
   Widget build(BuildContext context) {
-    final screens = [
-      const HomeScreen(),
-      const MapsOptionsScreen(),
-      const SettingsScreen(),
-    ];
-
     return Scaffold(
       body: IndexedStack(
         index: selectedIndex,
         children: screens,
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.shifting,
-        currentIndex: selectedIndex,
-        onTap: (value) {
-          setState(() {
-            selectedIndex = value;
-          });
-        },
-        elevation: 0,
-        items: [
-          BottomNavigationBarItem(
-            icon: FadeInLeft(child: const FaIcon(FontAwesomeIcons.house)),
-            activeIcon: BounceInDown(
-              child: const FaIcon(FontAwesomeIcons.house),
-            ),
-            label: 'Inicio',
-            tooltip: 'Inicio',
-            backgroundColor: const Color.fromARGB(255, 9, 27, 43),
-          ),
-          BottomNavigationBarItem(
-            icon:
-                FadeInUp(child: const FaIcon(FontAwesomeIcons.mapLocationDot)),
-            activeIcon: BounceInDown(
-                child: const FaIcon(FontAwesomeIcons.mapLocationDot)),
-            label: 'Mapas',
-            tooltip: 'Mapas',
-            backgroundColor: const Color.fromARGB(255, 9, 27, 43),
-          ),
-          BottomNavigationBarItem(
-            icon: FadeInRight(
-              child: const FaIcon(FontAwesomeIcons.gear),
-            ),
-            activeIcon:
-                BounceInDown(child: const FaIcon(FontAwesomeIcons.gear)),
-            label: 'Ajustes',
-            tooltip: 'Ajustes',
-            backgroundColor: const Color.fromARGB(255, 9, 27, 43),
-          ),
-        ],
+      bottomNavigationBar: CurvedNavigationBar(
+          index: selectedIndex,
+          height: 60,
+          items: [
+            _builNavItem(Icons.home, selectedIndex == 0),
+            _builNavItem(Icons.map, selectedIndex == 1),
+            _builNavItem(Icons.settings, selectedIndex == 1),
+          ],
+          color: const Color.fromARGB(255, 9, 27, 43),
+          buttonBackgroundColor: const Color.fromARGB(255, 9, 27, 43),
+          backgroundColor: Colors.grey.shade300,
+          animationCurve: Curves.easeInOut,
+          animationDuration: const Duration(milliseconds: 300),
+          onTap: (value) {
+            setState(() {
+              selectedIndex = value;
+            });
+          }),
+    );
+  }
+
+  Widget _builNavItem(IconData icon, bool isSelected) {
+    return Padding(
+      padding: const EdgeInsets.all(6.0),
+      child: Icon(
+        icon,
+        size: 30,
+        color: isSelected ? const Color(0xFFD3DDDF) : Colors.white,
       ),
     );
   }
